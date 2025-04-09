6cdb3eb1aaa7f72150b6ed93224573ab7d1106b9
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2025-04-09 09:44:33 +0200

    Add support for test id and path-based omit and hash.

--- testbed/Run-MonitorSession.ps1
+++ testbed/Run-MonitorSession.ps1
@@ -62,6 +62,7 @@ function Convert-RepeatInterval {
     }
 }
 
+#------------------------------------------------------------------------------
 # Test if the input $TestSuiteFile exists
 if (-not (Test-Path -Path $TestSuiteFile)) {
     Write-Error "The configuration file '$TestSuiteFile' does not exist."
@@ -110,15 +111,15 @@ try {
             $randomDelay = Get-Random -Minimum 1 -Maximum $intervalSeconds
 
             $jobs += @{
-                ResultsFile   = $OutPath + "/" + $test["write-to"] 
-                PythonCmd     = $pythonCmd
-                Configuration = $targetJson
-                Interval      = $intervalSeconds
-                Location      = "$rootFolder/$testPath"
-                Id            = ++$testId
-                Name          = $test.test
-                LastRun       = (Get-Date).AddSeconds($randomDelay)
-                JobObject     = $null
+                ResultsFile    = $OutPath + "/" + $test["write-to"] 
+                PythonCmd      = $pythonCmd
+                Configuration  = $targetJson
+                Interval       = $intervalSeconds
+                Location       = "$rootFolder/$testPath"
+                Id             = ++$testId
+                Name           = $test.test
+                LastRun        = (Get-Date).AddSeconds($randomDelay)
+                JobObject      = $null
                 OmitProperties = $test["omit-fields"]
                 HashProperties = $test["hash-fields"]
             }
@@ -139,8 +140,80 @@ try {
             # Check if the job is due to run.
             if (($now - $job.LastRun).TotalSeconds -ge $job.Interval -and $null -eq $job.JobObject) {
                 Write-Host "  Launching $($job.Name).$($job.Id), seconds since last run: $(($now - $job.LastRun).TotalSeconds), expected $($job.Interval)"
+                
                 # Start the external Python script.
-                $job.JobObject = Start-ThreadJob -StreamingHost $Host -ScriptBlock {
+                $job.JobObject = Start-ThreadJob -StreamingHost $Host -ScriptBlock {                    
+                    #-- Need to have these two function accessbile from this script block ----------------
+                    function OmitProperty {
+                        param (
+                            [Parameter(Mandatory = $true)]
+                            [PSObject]$Object,
+        
+                            [Parameter(Mandatory = $true)]
+                            [string]$Property
+                        )
+
+                        # Split the property path into individual components
+                        $properties = $Property -split '\.'
+
+                        # Iterate through each level of the object hierarchy
+                        $currentObject = $Object
+                        for ($i = 0; $i -lt $properties.Length - 1; $i++) {
+                            # Move to the next level of the object (i.e., go deeper into the nested objects)
+                            $currentObject = $currentObject.PSObject.Properties[$properties[$i]].Value
+                        }
+
+                        # Remove the final property from the last level of the object
+                        $currentObject.PSObject.Properties.Remove($properties[-1])
+                    }
+
+                    function HashProperty {
+                        param (
+                            [Parameter(Mandatory = $true)]
+                            [PSObject]$Object,
+
+                            [Parameter(Mandatory = $true)]
+                            [string]$SourceProperty,
+
+                            [Parameter(Mandatory = $true)]
+                            [string]$TargetProperty
+                        )
+
+                        # Split the source property path into individual components
+                        $sourceProperties = $SourceProperty -split '\.'
+    
+                        # Traverse the object to access the source property
+                        $currentObject = $Object
+                        for ($i = 0; $i -lt $sourceProperties.Length - 1; $i++) {
+                            # Go deeper into the nested objects
+                            $currentObject = $currentObject.PSObject.Properties[$sourceProperties[$i]].Value
+                        }
+
+                        # Get the value of the source property
+                        $sourceValue = $currentObject.PSObject.Properties[$sourceProperties[-1]].Value
+
+    
+                        # Compute the hash of the source property value (you can use any hash algorithm, here we use SHA256)
+                        $hash = [System.Security.Cryptography.MD5]::Create().ComputeHash([System.Text.Encoding]::UTF8.GetBytes($sourceValue)) 
+                        $hashString = [BitConverter]::ToString($hash) -replace '-'
+
+                        # Split the target property path into individual components
+                        $targetProperties = $TargetProperty -split '\.'
+    
+                        # Traverse the object to access or create the target property
+                        $currentObject = $Object
+                        for ($i = 0; $i -lt $targetProperties.Length - 1; $i++) {
+                            if (-not $currentObject.PSObject.Properties[$targetProperties[$i]]) {
+                                # If the property doesn't exist, create it
+                                $currentObject | Add-Member -MemberType NoteProperty -Name $targetProperties[$i] -Value @{}
+                            }
+                            # Go deeper into the nested objects
+                            $currentObject = $currentObject.PSObject.Properties[$targetProperties[$i]].Value
+                        }
+                        $currentObject | Add-Member -Name $targetProperties[-1] -Type NoteProperty -Value $hashString
+                        # Set the hash value in the target property
+                    }                   
+                    #------------------------------------
                     $job = $using:job
                     $cmd = $job.PythonCmd -replace '"', "'"
                     $path = $job.Location
@@ -155,26 +228,15 @@ try {
                     # Add timestamp property:
                     $timestamp = $(Get-Date).ToString("yyyy-MM-ddTHH:mm:ss")
                     $jsonObject | Add-Member -Name 'Timestamp' -Type NoteProperty -Value $timestamp 
-                    # Add source property:
-                    # TODO: where to get this value? environment variable? which one?
+                    # Add test instance ID property:
+                    $jsonObject | Add-Member -Name 'TestId' -Type NoteProperty -Value "$($job.Name).$($job.Id)" 
 
-                    # Hash properties:
-                    foreach($hashProp in $job.HashProperties) {
-                        $md5 = $using:md5
-                        $utf8 = $using:utf8
-                        $str = $jsonObject."$($hashProp.src)"
-                        if ($null -ne $str)
-                        {                    
-                            $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($str))).Replace('-','')
-                            $jsonObject | Add-Member -Name $hashProp.trg -Type NoteProperty -Value $hash
-                        }
-                        else {
-                            Write-Host "  Error: Hash: Cannot make hash: $($hashProp.src) -> $($hashProp.trg). Property $($hashProp.src) does not exists or is empty."
-                        }
-                    }                    
+                    foreach ($hashProp in $job.HashProperties) {
+                        HashProperty -Object $jsonObject -SourceProperty $hashProp.src -TargetProperty $hashProp.trg
+                    }                  
                     # Omit properties:
-                    foreach($omit in $job.OmitProperties) {
-                        $jsonObject.PSObject.Properties.Remove($omit) 
+                    foreach ($omit in $job.OmitProperties) {
+                        OmitProperty -Object $jsonObject -Property $omit
                     }
 
                     $jsonObject | ConvertTo-Json -Compress | Add-Content -Path $outfilePath
