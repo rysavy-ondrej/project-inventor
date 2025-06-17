85879e4f16d672442a8ceff72967103a11ffa425
Petr Matousek <matousp@pcmatousek.fit.vutbr.cz>
2025-06-17 16:51:19 +0200

    ssh tests added

--- testbed/inventor-testbed.run-all.sh
+++ testbed/inventor-testbed.run-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.ping.yaml -OutPath ./out/ &
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.smtp.yaml -OutPath ./out/ &
+pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/security.ssh.yaml -OutPath ./out/ &
 PID1=$!
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/webapp.http.yaml -OutPath ./out/
 #PID2=$!
