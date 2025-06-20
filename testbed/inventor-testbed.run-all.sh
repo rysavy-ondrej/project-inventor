0b91b8221299c0935968ea36a3e053c07e12713c
Petr Matousek <matousp@pcmatousek.fit.vutbr.cz>
2025-06-20 10:43:43 +0200

    DNS monitor added

--- testbed/inventor-testbed.run-all.sh
+++ testbed/inventor-testbed.run-all.sh
@@ -3,6 +3,7 @@ pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.ping.yaml -OutPath 
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.smtp.yaml -OutPath ./out/ &
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/security.ssh.yaml -OutPath ./out/ &
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.imap.yaml -OutPath ./out/ &
+pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/network.dns.yaml -OutPath ./out/ &
 PID1=$!
 pwsh Run-MonitorSession.ps1 -TestSuiteFile schedules/webapp.http.yaml -OutPath ./out/
 #PID2=$!
